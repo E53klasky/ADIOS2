a79cac152bc8ba5ee2091712c7eff39046e43691
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-10-19 11:07:45 -0400

    I am added the header files and something which i acna esasily now do so i can get good work on it

--- source/adios2/operator/compress/CompressCAESAR.cpp
+++ source/adios2/operator/compress/CompressCAESAR.cpp
@@ -1,1 +1,3 @@
 #include "CompressCAESAR.h"
+#include "adios2/helper/adiosFunctions.h"
+#include "caesar/models/compress/compressor.h"
--- source/adios2/operator/compress/CompressCAESAR.h
+++ source/adios2/operator/compress/CompressCAESAR.h
@@ -1,3 +1,24 @@
 #ifndef ADIOS2_OPERATOR_COMPRESS_COMPRESS_CAESAR_H_
 #define ADIOS2_OPERATOR_COMPRESS_COMPRESS_CAESAR_H_
-#endif 
+
+#include "adios2/core/Operator.h"
+
+namespace adios2
+{
+    namespace core
+    {
+        namespace compress
+        {
+
+            class CompressCAESAR : public Operator
+            {
+            public:
+                CompressCAESAR(const Params& parameters);
+                ~CompressCAESAR() override = default;
+            };
+
+        } // namespace compress
+    } // namespace core
+} // namespace adios2
+
+#endif // ADIOS2_OPERATOR_COMPRESS_COMPRESS_CAESAR_H_
