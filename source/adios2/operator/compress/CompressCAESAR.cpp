7e04b754839b61032400882a9da98355257b613e
Ethan Klasky <173562962+E53klasky@users.noreply.github.com>
2025-10-19 18:33:11 -0400

    added the core i will add spefics

--- source/adios2/operator/compress/CompressCAESAR.cpp
+++ source/adios2/operator/compress/CompressCAESAR.cpp
@@ -1,3 +1,41 @@
 #include "CompressCAESAR.h"
 #include "adios2/helper/adiosFunctions.h"
 #include "caesar/models/compress/compressor.h"
+
+namespace adios2
+{
+    namespace core
+    {
+        namespace compress
+        {
+            CompressCAESAR::CompressCAESAR(const Params& parameters)
+                : Operator("caesar" , COMPRESS_CAESAR , "compress" , parameters)
+            {
+            }
+            size_t CompressCAESAR::Operate(const char* dataIn , const Dims& blockStart , const Dims& blockCount ,
+                const DataType type , char* bufferOut)
+            {
+                return 0;
+            }
+            size_t CompressCAESAR::InverseOperate(const char* bufferIn , const size_t sizeIn , char* dataOut)
+            {
+                return 0;
+            }
+
+            // TODO: check what other values work i have tried double and float thus far 
+            bool CompressCAESAR::IsDataTypeValid(const DataType type) const
+            {
+                if (type == DataType::Double || type == DataType::Float)
+                {
+                    return true;
+                }
+                return false;
+            }
+            size_t CompressCAESAR::DecompressV1(const char* bufferIn , const size_t sizeIn , char* dataOut)
+            {
+                return 0;
+            }
+
+        } // end namespace compress
+    } // end namespace core
+} // end namespace adios2
--- source/adios2/operator/compress/CompressCAESAR.h
+++ source/adios2/operator/compress/CompressCAESAR.h
@@ -1,8 +1,7 @@
 #ifndef ADIOS2_OPERATOR_COMPRESS_COMPRESS_CAESAR_H_
 #define ADIOS2_OPERATOR_COMPRESS_COMPRESS_CAESAR_H_
-
 #include "adios2/core/Operator.h"
-
+// TODO: find out what other methdos and variables are needed this is what both MGARD and SZ have
 namespace adios2
 {
     namespace core
@@ -13,8 +12,35 @@ namespace adios2
             class CompressCAESAR : public Operator
             {
             public:
+                 /**
+     * @param dataIn
+     * @param blockStart
+     * @param blockCount
+     * @param type
+     * @param bufferOut
+     * @return size of compressed buffer
+     */
                 CompressCAESAR(const Params& parameters);
                 ~CompressCAESAR() override = default;
+
+                size_t Operate(const char* dataIn , const Dims& blockStart , const Dims& blockCount ,
+                    const DataType type , char* bufferOut) final;
+
+                size_t InverseOperate(const char* bufferIn , const size_t sizeIn , char* dataOut) final;
+                bool IsDataTypeValid(const DataType type) const final;
+
+            private:
+                /**
+     * Decompress function for V1 buffer. Do NOT remove even if the buffer
+     * version is updated. Data might be still in lagacy formats. This function
+     * must be kept for backward compatibility
+     * @param bufferIn : compressed data buffer (V1 only)
+     * @param sizeIn : number of bytes in bufferIn
+     * @param dataOut : decompressed data buffer
+     * @return : number of bytes in dataOut
+     */
+                size_t DecompressV1(const char* bufferIn , const size_t sizeIn , char* dataOut); // idk what this is
+                std::string m_VersionInfo; // this is the model
             };
 
         } // namespace compress
